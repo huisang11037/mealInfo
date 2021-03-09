d0746b077a665cb7459dec632b71f5541e82e66d
BuildTools <unconfigured@null.spigotmc.org>
2021-03-10 00:07:55 +0900

    현재 시간에 맞춰서 조식/중식/석식 중 하나가 나오도록 변경. UI 다듬기.

--- app/src/main/java/o/huisang26/mealinfo/MainActivity.java
+++ app/src/main/java/o/huisang26/mealinfo/MainActivity.java
@@ -22,6 +22,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.net.URL;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -173,9 +174,41 @@ public class MainActivity extends AppCompatActivity {
         String dDISH_NM = null, mMeal_SC_NM = null;
         String code = null;
         boolean inCode = false;
+
+        String[] mealList = {"조식", "중식", "석식"};
+        String curMeal = "";
+        Calendar currentTime = Calendar.getInstance();
+        currentTime.setTimeInMillis(System.currentTimeMillis());
+
+        //아침, 점심, 저녁 순
+        Calendar[] mealTime = {Calendar.getInstance(), Calendar.getInstance(), Calendar.getInstance()};
+        mealTime[0].setTimeInMillis(System.currentTimeMillis());
+        mealTime[0].set(Calendar.MINUTE, 20);
+        mealTime[0].set(Calendar.HOUR_OF_DAY, 8);
+        mealTime[1].setTimeInMillis(System.currentTimeMillis());
+        mealTime[1].set(Calendar.MINUTE, 40);
+        mealTime[1].set(Calendar.HOUR_OF_DAY, 12);
+        mealTime[2].setTimeInMillis(System.currentTimeMillis());
+        mealTime[2].set(Calendar.MINUTE, 30);
+        mealTime[2].set(Calendar.HOUR_OF_DAY, 6);
+
+        if (currentTime.getTimeInMillis() - mealTime[2].getTimeInMillis() > 0){ //저녁 시간이 넘었는가?
+            currentTime.add(Calendar.DATE, 1);
+            DateFormat df = new SimpleDateFormat("yyyyMMdd");
+            date_text = df.format(currentTime.getTime());
+            curMeal = mealList[0];
+        } else if(currentTime.getTimeInMillis() - mealTime[1].getTimeInMillis() > 0){ //점심 시간이 넘었는가?
+            curMeal = mealList[2];
+        } else if(currentTime.getTimeInMillis() - mealTime[0].getTimeInMillis() > 0){ //아침 시간이 넘었는가?
+            curMeal = mealList[1];
+        } else{
+            curMeal = mealList[0];
+        }
+
         diteView.setText(date_text.substring(0, 4) + "년 " +
                 date_text.substring(4, 6) + "월 " +
                 date_text.substring(6, 8) + "일\n\n");
+
         try{
             URL url = new URL("https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=178a8938c5404e889f3f20eee3811ae0&Type=xml" +
                     "&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8881025" +
@@ -224,20 +257,22 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                         if(parser.getName().equals("MMEAL_SC_NM")){
-                            diteView.setText(diteView.getText() + mMeal_SC_NM + "\n\n");
+                            if (mMeal_SC_NM.equals(curMeal))
+                                diteView.setText(diteView.getText() + mMeal_SC_NM + "\n\n");
                         }
                         if(parser.getName().equals("DDISH_NM")){
-                            //원래 문자열을 깔끔하게 정리하기 위한 과정
-                            String dish = dDISH_NM.replaceAll("[0-9]", "");
-                            dish = dish.replace(".", "");
-                            dish = dish.replace("*", "");
-                            dish = dish.replace("<br/>", "/");
-                            String[] dishs = dish.split("/"); //각 급식 항목들을 dishs[]에 하나씩 넣음
+                            if (mMeal_SC_NM.equals(curMeal)) {
+                                //원래 문자열을 깔끔하게 정리하기 위한 과정
+                                String dish = dDISH_NM.replaceAll("[0-9]", "");
+                                dish = dish.replace(".", "");
+                                dish = dish.replace("*", "");
+                                dish = dish.replace("<br/>", "/");
+                                String[] dishs = dish.split("/"); //각 급식 항목들을 dishs[]에 하나씩 넣음
 
-                            for (int i =0; i < dishs.length; i++){
-                                diteView.setText(diteView.getText() + dishs[i] + "\n");
+                                for (int i = 0; i < dishs.length; i++) {
+                                    diteView.setText(diteView.getText() + dishs[i] + "\n");
+                                }
                             }
-                            diteView.setText(diteView.getText() + "\n\n\n");
                         }
 
                         break;
@@ -256,7 +291,7 @@ public class MainActivity extends AppCompatActivity {
         timetableView.setText(null);
         String currentGrade = gradeSpinner.getSelectedItem().toString();
         boolean isHigh = true;
-        if (currentGrade.substring(0, 2).equals("중")){
+        if (currentGrade.charAt(0) == '중'){
             isHigh = false;
         }
         int grade = Integer.parseInt(currentGrade.substring(1));
@@ -307,7 +342,7 @@ public class MainActivity extends AppCompatActivity {
                             timetableView.setText(timetableView.getText() + perio + "교시: ");
                         }
                         if(parser.getName().equals("ITRT_CNTNT")){
-                            timetableView.setText(timetableView.getText() + itrt_CNTNT + "\n");
+                            timetableView.setText(timetableView.getText() + itrt_CNTNT.replace("-", "") + "\n");
                         }
                         break;
                 }
